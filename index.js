6153c60a162d07501746312aa54973f649672a50
David Adedeji <97545459+Dadedeji8@users.noreply.github.com>
2026-03-19 19:23:42 +0100

    update

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(express.json());
 
-app.use(cors());
+app.use(cors("*"));
 app.use("/api/email", emailRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/user", protect, userRoutes);
